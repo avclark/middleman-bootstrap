e3a3eda863a6763c45dd1f0f53df777e31a8bdee
Adam Clark <avclark@gmail.com>
2011-08-03 22:47:17 -0400

    Added support of CCS3 PIE and Selectivizr and enabled 960.gs by default.

--- config.rb
+++ config.rb
@@ -2,7 +2,7 @@
 set :haml, { :format => :html5 }
 
 # Compass 960.gs support
-# require 'ninesixty'
+require 'ninesixty'
 
 # CodeRay syntax highlighting in Haml
 # activate :code_ray
@@ -39,20 +39,20 @@ end
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+    activate :minify_css
   
   # Minify Javascript on build
-  # activate :minify_javascript
+    activate :minify_javascript
   
   # Enable cache buster
   # activate :cache_buster
   
   # Use relative URLs
-  # activate :relative_assets
+    activate :relative_assets
   
   # Compress PNGs after build (gem install middleman-smusher)
-  # require "middleman-smusher"
-  # activate :smusher
+    require "middleman-smusher"
+    activate :smusher
 
   # Generate ugly/obfuscated HTML from Haml
   # activate :ugly_haml
